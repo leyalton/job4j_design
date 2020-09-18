25277eb25671fc3d0d316d9940395361cc4e5333
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-09-18 17:30:32 +0300

    0.4. BufferedOutputStream[252490#334467]

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,26 +1,38 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LogFilter {
-    public static boolean filter(String file) {
-        return file.contains("404");
-    }
-
-    public static void main(String[] args) {
-        try (BufferedReader in = new BufferedReader(new FileReader("log.txt"))) {
+    public static List<String> filter(String file) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             List<String> lines = new ArrayList<>();
             in.lines().forEach(lines::add);
             for (String line : lines) {
-                if (filter(line)){
-                    System.out.println(line);
+                if (file.contains("404")) {
+                    result.add(line);
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return result;
+    }
+
+    public static void save(List<String> log, String file) {
+        try (PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String line : log) {
+                printWriter.write(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        save(log, "404.txt");
     }
 }
