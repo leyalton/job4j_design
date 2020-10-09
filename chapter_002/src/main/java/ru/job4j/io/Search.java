6d8b2016978b61ae738f2847bd37f76320155ba0
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-10-10 00:07:28 +0300

    5. Валидация параметров запуска.[246865#334464]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,8 +9,13 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "txt").forEach(System.out::println);
+        if (args[0] == null || args[1] == null) {
+            throw new IllegalArgumentException("Root folder or extension to find is null. Use java -jar dir.jar ROOT_FOLDER EXTENSION_TO_FIND");
+        }
+        File path = new File(args[0]);
+        File ext = new File(args[1]);
+        Path start = Paths.get(String.valueOf(path));
+        search(start, String.valueOf(ext)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
