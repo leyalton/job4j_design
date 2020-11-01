8f5e5f4eec6bf82227a1254b8cfaac8b9f0c3c5c
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-11-02 01:14:47 +0300

    1. Бот[7921#334488]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,30 +1,36 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 public class EchoServer {
 
-    public static void main(String[] args) throws IOException {
-        boolean rst = true;
+    public void server() {
         try (ServerSocket server = new ServerSocket(9000)) {
-
             while (true) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    String line;
+                    String message = null;
+                    while (!(line = in.readLine()).isEmpty()) {
+                        if (line.contains("msg=") && line.contains("exit")) {
                             return;
+                        } else if (line.contains("msg=")) {
+                            message = line.substring(line.indexOf("=") + 1, line.lastIndexOf(" "));
                         }
+                        System.out.println(line);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(message.getBytes());
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        new EchoServer().server();
     }
 }
