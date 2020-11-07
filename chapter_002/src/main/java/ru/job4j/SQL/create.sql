4d3d7ba356d571c845dcde17daba7690c522c6eb
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-11-07 17:32:22 +0300

    1. Схема прав пользователи и роли.[1093#334397]

--- chapter_002/src/main/java/ru/job4j/SQL/create.sql
+++ chapter_002/src/main/java/ru/job4j/SQL/create.sql
@@ -1,51 +1,51 @@
 create database tracker;
 
 create table rule(
-                     rule_id   serial primary key,
-                     rule_name varchar(255) not null
+    rule_id   serial primary key,
+    rule_name varchar(255) not null
 );
 
 create table role(
-                     role_id    serial primary key,
-                     role_name  varchar(255) not null,
-                     role_rule int references rule (rule_id)
+    role_id   serial primary key,
+    role_name varchar(255) not null,
+    role_rule int references rule (rule_id)
 );
 
 create table users(
-                      user_id   serial primary key,
-                      user_name varchar(255),
-                      user_role int references role (role_id)
+    user_id   serial primary key,
+    user_name varchar(255),
+    user_role int references role (role_id)
 );
 
-create table item_category
-    category_id   serial primary key,
+create table item_category(
+    category_id serial primary key,
     category_name varchar(255) not null
-    );
+);
 
 create table item_state(
-                           state_id   serial primary key,
-                           state_name varchar(255) not null
+    state_id   serial primary key,
+    state_name varchar(255) not null
 );
 
 create table item_attach(
-                            attach_id   serial primary key,
-                            attach_name varchar(255) not null
+    attach_id   serial primary key,
+    attach_name varchar(255) not null
 );
 
 create table item(
-                     item_id     serial primary key,
-                     item_name   varchar(255) not null,
-                     item_author int references users (user_id),
-                     item_category   int references item_category (category_id),
-                     item_state  int references item_state (state_id),
-                     item_file   int references item_attach (attach_id)
+    item_id       serial primary key,
+    item_name     varchar(255) not null,
+    item_author   int references users (user_id),
+    item_category int references item_category (category_id),
+    item_state    int references item_state (state_id),
+    item_file     int references item_attach (attach_id)
 );
 
 create table item_comments(
-                              comment_id serial primary key,
-                              comment    varchar(255),
-                              author     int references users (user_id),
-                              item       int references item (item_id)
+    comment_id serial primary key,
+    comment    varchar(255),
+    author     int references users (user_id),
+    item       int references item (item_id)
 );
 
 insert into role(role_id, role_name) values (1, 'role 1');
