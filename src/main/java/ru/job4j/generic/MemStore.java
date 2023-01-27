94ff14ea42daf11b802ce57dd6ecb4c1459a1cce
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2023-01-27 22:52:48 +0300

    Реализовать Store<T extends Base> refactor

--- src/main/java/ru/job4j/generic/MemStore.java
+++ src/main/java/ru/job4j/generic/MemStore.java
@@ -8,31 +8,35 @@ public final class MemStore<T extends Base> implements Store<T> {
 
     @Override
     public void add(T model) {
-        if (!storage.containsKey(model.getId())) {
-            storage.put(model.getId(), model);
+        if (storage.containsKey(model.getId())) {
+            return;
         }
+        storage.put(model.getId(), model);
+
     }
 
     @Override
     public boolean replace(String id, T model) {
+        boolean result = false;
         if (storage.containsKey(id)) {
             storage.put(id, model);
-            return true;
+            result = true;
         }
-        return false;
+        return result;
     }
 
     @Override
     public boolean delete(String id) {
+        boolean result = false;
         if (storage.containsKey(id)) {
             storage.remove(id);
-            return true;
+            result = true;
         }
-        return false;
+        return result;
     }
 
     @Override
     public T findById(String id) {
-        return storage.containsKey(id) ? storage.get(id) : null;
+        return storage.getOrDefault(id, null);
     }
 }
